ab841b493781b3923615bf60223aedc70c7d354d
woody huang <2423847695@qq.com>
2017-09-06 17:30:57 +0800

    remove other lang remove browseServer

--- app/controllers/ckeditor/pictures_controller.rb
+++ app/controllers/ckeditor/pictures_controller.rb
@@ -1,12 +1,13 @@
 class Ckeditor::PicturesController < Ckeditor::ApplicationController
 
   def index
-    @pictures = Ckeditor.picture_adapter.find_all(ckeditor_pictures_scope)
-    @pictures = Ckeditor::Paginatable.new(@pictures).page(params[:page])
-
-    respond_to do |format|
-      format.html { render :layout => @pictures.first_page? }
-    end
+    return render :text => 'access denied.', :status => 401
+    # @pictures = Ckeditor.picture_adapter.find_all(ckeditor_pictures_scope)
+    # @pictures = Ckeditor::Paginatable.new(@pictures).page(params[:page])
+    #
+    # respond_to do |format|
+    #   format.html { render :layout => @pictures.first_page? }
+    # end
   end
 
   def create
